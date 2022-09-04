7dd602bf69f45d662f7ca70508d54d4e5b62afb7
BunyMan <jpbuny@gmail.com>
2022-09-04 16:45:12 +0100

    cleanup

--- delete.py
+++
@@ -1,9 +0,0 @@
-from main import Session, User, engine
-
-local_session = Session(bind=engine)
-
-user_to_delete=local_session.query(User).filter(User.username== "BunyMan").first()
-
-local_session.delete(user_to_delete)
-
-local_session.commit()
